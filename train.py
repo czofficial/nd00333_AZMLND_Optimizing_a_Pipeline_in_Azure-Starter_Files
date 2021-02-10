36644d289ac5bfd457fa18a440695880ae0b35fc
cz.official <cz.official@MacBook-Pro-2017-CZ.local>
2021-02-10 15:58:17 +0100

    Updated README.md, train.py and udacity-project.ipynb

--- train.py
+++ train.py
@@ -10,19 +10,8 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+# Create TabularDataset using TabularDatasetFactory
+ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv") 
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,7 +38,14 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df, y_df
+
+# Split data into train and test sets.
+x, y = clean_data(ds)
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
@@ -67,6 +63,9 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
